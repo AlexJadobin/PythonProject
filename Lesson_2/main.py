9d66cb4f581ba6ddb48e5c39d0b242edb35abd8d
AlexJadobin <iskj@yandex.ru>
2021-12-03 21:31:05 +0700

    практическое_занятие_2 Добавил выполнение пункта 3

--- Lesson_2/main.py
+++ Lesson_2/main.py
@@ -1,7 +1,19 @@
 # №2
 string = "Быстрый коричнивый лис прыгает над ленивой собакой"
 print(string)
+print("")
 # №3
 newstring = ["Быстрый", "коричневый", "лис", "прыгает", "над", "ленивой", "собакой"]
 for elem in newstring:
     print(elem)
+print("")
+
+print(string.split())
+print("")
+for elem in string.split():
+    print(elem)
+print("")
+new1string = (string.split())
+for elem in newstring:
+    print(elem)
+
