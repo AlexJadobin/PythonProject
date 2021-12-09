2de8236911970ec09f120f7368657f3ee04155e1
AlexJadobin <iskj@yandex.ru>
2021-12-10 02:38:23 +0700

    Занятие №4, пункт №1 формирование списка кубов целых чисел добавил варианат №2 и №3

--- homework_04/main.py
+++ homework_04/main.py
@@ -15,10 +15,31 @@ def prostye(n):
     return
 
 prostye(f)
+
 print("выбор простых чисел до значения =", f, 'в список sps_prost')
 print(sps_prost)
 print('--')
 print("расчёт кубов простых чисел до значения =", f, 'в список kub_sps_prost')
 print(kub_sps_prost)
 print('____________')
+
+# №4 вариант №2
+def kub_number(*numbers, power=3):
+    return [num ** power for num in numbers]
+
+print("расчет и печать кубов целых чисел ")
+print(kub_number(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
+print('--')
+
+# №4 вариант №3
+kub_sps = []
+def kub_number(*numbers, power=3):
+    for j in numbers:
+        i = j ** power
+        kub_sps.append(i)
+    return
+
+kub_number(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+print("расчет, формирование списка [kub_sps] кубов целых чисел и печать списка")
+print(kub_sps)
 
