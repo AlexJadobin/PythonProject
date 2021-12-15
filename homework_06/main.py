cec0724fc6458c7e4ee05da9a1546dab05dc8201
AlexJadobin <iskj@yandex.ru>
2021-12-16 01:29:17 +0700

    Занятие №6

--- homework_06/main.py
+++ homework_06/main.py
@@ -5,7 +5,24 @@ class Car:
         self.fuel = fuel        # int, условное количество оставшегося топлива
         self.fuel_consumption = fuel_consumption    # 5. int, условное значение,
         # сколько единиц топлива расходуется на единицу расстояния
-    def start(self)
+    def start(self):
+        if self.started == 'заведено':
+            print('Можно ехать')
+            return True
+        else:
+            print('Авто не заведен, ехать нельзя, необходимо проверить уровень топлива')
+            self.started = False
+        if self.started == False:
+            if self.fuel > 0:
+                self.started = True
+                print('Топлива имеется, авто заведен')
+                return True
+            else:
+                print('Авто не завелся, так как остаток топлива = ', self.fuel)
+moy_car = Car(800,'незаведено',25,10)
+print(moy_car.start())
+
+
 
 
 
