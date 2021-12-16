2459ddc3f839fad00f6486d58aac57fd3cc956e8
AlexJadobin <iskj@yandex.ru>
2021-12-17 02:40:37 +0700

    Занятие №6_move

--- homework_06/main.py
+++ homework_06/main.py
@@ -1,3 +1,10 @@
+# объявление исключений
+class LowFuelError(Exception):
+    pass
+
+class NotEnoughFuel(Exception):
+    pass
+
 class Car:
     def __init__(self, weight, started, fuel, fuel_consumption):    # инициализация
         self.weight = weight    # int, вес автомобиля
@@ -6,21 +13,60 @@ class Car:
         self.fuel_consumption = fuel_consumption    # 5. int, условное значение,
         # сколько единиц топлива расходуется на единицу расстояния
     def start(self):
-        if self.started == 'заведено':
-            print('Можно ехать')
-            return True
+        try:
+            if not(self.started):
+                if self.fuel > 0:
+                    self.started = 1
+                    print('двигатель заведен')
+                    #return True
+                else:
+                    raise LowFuelError
+        except(LowFuelError):
+            print('Исключение. Топлива нет, толкаем авто до заправки')
+            return False
         else:
-            print('Авто не заведен, ехать нельзя, необходимо проверить уровень топлива')
-            self.started = False
-        if self.started == False:
-            if self.fuel > 0:
-                self.started = True
-                print('Топлива имеется, авто заведен')
-                return True
-            else:
-                print('Авто не завелся, так как остаток топлива =  ', self.fuel)
-moy_car = Car(800,'незаведено',25,10)
-print(moy_car.start())
+            print('Автомобиль готов, топливо:',self.fuel,'литров, можно ехать')
+        finally:
+            print('Хорошо покатались')
+            print('---')
+
+    def move(self,distance):
+        try:
+            if self.fuel < self.fuel_consumption * distance:
+                raise NotEnoughFuel
+        except(NotEnoughFuel):
+            #self.fuel -= self.fuel_consumption * distance
+            print('Исключение: недостаточно топлива на поездку',self.fuel,'литров, необходимо',self.fuel_consumption * distance, 'литров, а лучше полный бак!!!')
+            print('---------')
+        else:
+            self.fuel -= self.fuel_consumption * distance
+            print('Проехали ', distance, ' км, Остаток топлива : ',self.fuel, ' литра(ов)')
+            print('---')
+
+print("Пример №1: если топлива было 50 литров, а расход 5 л/10 км, то проехав 10 км у нас остаётся топлива 0 литров")
+car = Car(weight=800,started=0,fuel=50,fuel_consumption=5)
+car.start()
+s = 10  # дистанция 10 километров
+car.move(s)
+print("")
+
+print("Пример №2: если топлива было 10 литров, а расход 3 л/км, то проехав 2 км, топлива должно остаться 4 литра")
+car = Car(weight=800,started=0,fuel=10,fuel_consumption=3)
+car.start()
+s = 2  # дистанция 2 километра
+car.move(s)
+
+print("Пример №3: если топлива было 0 литров, а нужно срочно ехать!!!")
+car = Car(weight=800,started=0,fuel=0,fuel_consumption=5)
+car.start()
+
+print("Пример №4: если топлива было 10 литров, а расход 3 л/км, а необходимо проехать 5 км, то куда мы доедем ?")
+car = Car(weight=800,started=0,fuel=10,fuel_consumption=3)
+s = 5  # дистанция 5 километра
+car.move(s)
+
+
+
 
 
 
