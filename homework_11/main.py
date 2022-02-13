4f46c79dc44a3f3fef0ff934e0c1a4d3356a0140
AlexJadobin <iskj@yandex.ru>
2022-02-14 02:01:11 +0700

    Занятие №11

---
+++ homework_11/main.py
@@ -0,0 +1,14 @@
+import logging
+from waitress import serve
+from books_api import app
+logging.basicConfig(level=logging.INFO)
+
+def main():
+    logging.info("Starting server")
+    serve(app, host='127.0.0.1', port=8080)
+
+if __name__ == '__main__':
+    main()
+
+#http http://127.0.0.1:8080/books
+#http http://127.0.0.1:8080/books/1
