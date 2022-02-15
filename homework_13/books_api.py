e203682be1009fb157d9f8dc678e38421415dd33
AlexJadobin <iskj@yandex.ru>
2022-02-16 03:14:08 +0700

    Домашнее Задание №14

---
+++ homework_13/books_api.py
@@ -0,0 +1,14 @@
+from falcon import App
+from timing_middleware import TimingMiddleware
+from books_views import BooksListResours, BookDetailsResource
+
+middlewares = [
+    TimingMiddleware(),
+]
+
+books_list = BooksListResours()
+book_details = BookDetailsResource()
+
+app = App(middleware = middlewares)
+app.add_route("/books", books_list)
+app.add_route("/books/{book_id}", book_details)
